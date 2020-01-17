dee0d66becb3abd430fa4d3e5ad07205d431d071
Jordan Finneran <jordanfinners@users.noreply.github.com>
2020-01-17 09:46:38 +0000

    Update app.js
    Adding command to get the list of posts

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const mediumToMarkdown = require('medium-to-markdown');
 const fs = require('fs');
 const username = ''
+
+// You can get a list of your posts by going to medium.com/@username and open dev tools and run this command (inserting your username below)
+// copy(Array.from(new Set(Array.from(document.querySelector('div.es.y').querySelectorAll('a')).filter(p => p.pathname.indexOf('username/') > -1).map(p => p.href.replace(/\?source=-+\d+-+/ig, ''))))))
+
+
 const posts = [
   ]
 
